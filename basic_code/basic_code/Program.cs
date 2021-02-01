1c9d39158bb658bef5f91decfd0a421322496fe2
Fahim Ahmmed Mojumder <ahmmedahmmed75@gmail.com>
2021-02-01 10:45:46 +0600

    basic operation

---
+++ basic_code/basic_code/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello fahim");
+            int a = 10;
+            Console.WriteLine("the variable is " + a);
+            Console.WriteLine("");
+
+            short s = 10;
+            int i;
+            i = s;
+
+            int k = 10;
+            short e;
+            e = (short)k;
+
+            float t = 12.11f;
+            int oi = (int)t;
+
+        }
+    }
+}
---
+++ firstcode/firstcode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 44, y = 4;
+            Console.WriteLine("hello world  "+x+" "+y);
+            Console.WriteLine("hello world  {0} {1} ",x,y);
+            int add = x + y;
+            Console.WriteLine("Addition of " + x + " and " + y+" is "+add );
+            int sub = x - y;
+            Console.WriteLine("Subtraction of " + x + " and " + y + " is "+sub);
+            int mul = x * y;
+            Console.WriteLine("multiplication of " + x + " and " + y + " is "+mul);
+            int div = x / y;
+            Console.WriteLine("division of " + x + " and " + y + " is "+div);
+        }
+    }
+}
