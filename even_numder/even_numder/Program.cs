adf3336aa9a314a6d8f3a2f076709e85cc490278
fahimprito <ahmmedahmmed75@gmail.com>
2021-02-08 09:33:09 +0600

    even and odd

---
+++ even_numder/even_numder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_numder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            for (n = 1; n < 21; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("even numbers are {0}", n);
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ sum of even and odd/sum of even and odd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_numder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,n=100,sum=0;
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+
+                 
+            }
+            Console.WriteLine("sum of even num {0}", sum);
+            sum = 0;
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += i;
+                }
+                
+            }
+            Console.WriteLine("sum of odd num {0}", sum);
+
+
+        }
+    }
+}
+
