a007aa685e142b51e50dd949d05a43963116601e
fahimprito <ahmmedahmmed75@gmail.com>
2021-02-08 11:00:35 +0600

    11 am

--- Lab task 1/Lab task 1/Triangle.cs
+++ Lab task 1/Lab task 1/Triangle.cs
@@ -8,5 +8,22 @@ namespace Lab_task_1
 {
     class Triangle
     {
+        private int x;
+        private int y;
+        private int z;
+
+        public void showInfo()
+        {
+            Console.WriteLine("1st edge {0}", x);
+            Console.WriteLine("1st edge {1}", y);
+            Console.WriteLine("1st edge {2}", z);
+        }
+        public void TestTriangle()
+        {
+            if (x == y && y == z)
+                Console.WriteLine("the triangle is equilateral");
+           
+        }
+
     }
 }
