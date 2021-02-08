b3f806c241d2045af2d4f5b941b5189f277fde4f
fahimprito <ahmmedahmmed75@gmail.com>
2021-02-08 10:31:02 +0600

    percentage and grade

---
+++ Lab task 1/Lab task 1/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_task_1
+{
+    class student
+    {
+        private String name;
+        private String id;
+        private String department;
+        private float cgpa;
+
+    }
+}
--- percentage and grade/percentage and grade/Program.cs
+++ percentage and grade/percentage and grade/Program.cs
@@ -14,6 +14,18 @@ namespace percentage_and_grade
             percentage = (phy + chem + bio + math + com) / 5;
             Console.WriteLine("percentage of grade {0}", percentage);
 
+            if(percentage>=90)
+                Console.WriteLine("Grade A+");
+            else if (percentage <= 89 && percentage >= 85)
+                Console.WriteLine("Grade A");
+            else if (percentage <= 84 && percentage >= 80)
+                Console.WriteLine("Grade B+");
+            else if (percentage <= 79 && percentage >= 75)
+                Console.WriteLine("Grade B");
+            else if (percentage <= 74 && percentage >= 50)
+                Console.WriteLine("Grade C+");
+            else if (percentage >= 49)
+                Console.WriteLine("Grade F");
 
 
         }
